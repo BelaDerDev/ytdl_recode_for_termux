cd2bb8dbca0291b714ae2e51951d35e7e1c544f8
BelaDerDev <49236169+BelaDerDev@users.noreply.github.com>
2021-12-02 16:13:29 +0100

    Update install_ytdl_recode.sh

--- install_ytdl_recode.sh
+++ install_ytdl_recode.sh
@@ -4,7 +4,8 @@ termux-setup-storage
 echo Bitte Enter Drücken
 read
 apt install -y python ffmpeg
-pip install yt-dlp ffmpeg-python ffmpeg
+pip install ffmpeg-python ffmpeg
+python3 -m pip install --no-deps -U yt-dlp
 mkdir ~/bin
 mkdir ~/storage/shared/Youtube
 cat ~/ytdl_recode_for_termux/ytdl_recode.sh > ~/bin/termux-url-opener
